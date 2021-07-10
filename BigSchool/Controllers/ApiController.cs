d64c3a25fb02249ae3319e9bdeff7403251e523d
Hai Dang <lehaidangxh@gmail.com>
2021-07-10 19:30:24 +0700

    commit

--- BigSchool/Controllers/ApiController.cs
+++ BigSchool/Controllers/ApiController.cs
@@ -1,6 +1,16 @@
-﻿namespace BigSchool.Controllers
+﻿using System;
+using System.Web.Http;
+
+namespace BigSchool.Controllers
 {
     public class ApiController
     {
+
+        private IHttpActionResult BadRequest(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+       
     }
 }
--- BigSchool/Controllers/AttendancesController.cs
+++ BigSchool/Controllers/AttendancesController.cs
@@ -1,15 +1,19 @@
 ﻿using BigSchool.Models;
 using Microsoft.AspNet.Identity;
+using BigSchool.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+
 using System.Security.Principal;
 using System.Web.Http;
+using BigSchool.DTOs;
 
 namespace BigSchool.Controllers
 {
+    [Authorize]
     public class AttendancesController : ApiController
     {
         private ApplicationDbContext _dbContext;
@@ -18,18 +22,42 @@ namespace BigSchool.Controllers
             _dbContext = new ApplicationDbContext();
         }
         [HttpPost]
-        public IHttpActionResult Attend([FromBody] int courseId, IPrincipal user)
+       public IHttpActionResult Attend(AttendanceDto attendanceDto)
         {
-
-
+            var userid = System.Web.HttpContext.Current.User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.Attendeeid == userid && a.CourseID == attendanceDto.CourseId))
+                return BadRequest(" the attendance already exit");
             var attendance = new Attendance
             {
-                CourseID = courseId,
-                Attendeeid = user.Identity.GetUserId()
+                CourseID = attendanceDto.CourseId,
+                Attendeeid = userid
             };
             _dbContext.Attendances.Add(attendance);
             _dbContext.SaveChanges();
             return Ok();
+
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int courseId)
+        {
+
+            var userid = System.Web.HttpContext.Current.User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.Attendeeid == userid && a.CourseID == courseId))
+                return BadRequest(" the attendance already exit");
+            var attendance = new Attendance
+            {
+                CourseID = courseId,
+                Attendeeid = userid
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+        private IHttpActionResult BadRequest(string v)
+        {
+            throw new NotImplementedException();
         }
 
         private IHttpActionResult Ok()
